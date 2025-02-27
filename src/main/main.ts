57aae9c4c5b4b48daf4796f34dd123b465be11e8
WillSuo <willsuosuo@gmail.com>
2025-02-27 22:48:59 +0800

    feat(UI): fix the npm error when start

--- src/main/main.ts
+++ src/main/main.ts
@@ -105,6 +105,7 @@ const createWindow = async () => {
         ? path.join(__dirname, 'preload.js')
         : path.join(__dirname, '../../.erb/dll/preload.js'),
       nodeIntegration: true,
+      sandbox: false,
     },
   });
 
